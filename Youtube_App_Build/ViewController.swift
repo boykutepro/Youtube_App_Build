50463bc6cf8bf5af653c4850226b3c745205d90d
Thiện Tùng <tungcppro@gmail.com>
2020-07-04 09:30:18 +0700

    create xcode proj

---
+++ Youtube_App_Build/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube_App_Build
+//
+//  Created by Thien Tung on 7/4/20.
+//  Copyright © 2020 Thien Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
